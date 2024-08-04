c2b1d2e4394b8cf9a6340ee22d573d9aab7a3970
Takeshi-mi <e.takeshi.miura@gmail.com>
2024-08-04 15:56:21 -0300

    laravel ui

--- routes/web.php
+++ routes/web.php
@@ -2,14 +2,11 @@
 
 use Illuminate\Support\Facades\Route;
 
-Route::view('/', 'welcome');
+Route::view('/', 'home');
 
-Route::view('dashboard', 'dashboard')
-    ->middleware(['auth', 'verified'])
-    ->name('dashboard');
 
-Route::view('profile', 'profile')
-    ->middleware(['auth'])
-    ->name('profile');
 
-require __DIR__.'/auth.php';
+
+Auth::routes();
+
+Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
