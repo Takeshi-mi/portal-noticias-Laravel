1d96f7b2828b6060bc5b663cd17648dbc6d335c1
Takeshi-mi <e.takeshi.miura@gmail.com>
2024-08-04 14:05:52 -0300

    Laravel breeze instalado
    Co-authored-by: imaterial-tree <naokirafael.miura@gmail.com>

--- vite.config.js
+++ vite.config.js
@@ -5,9 +5,9 @@ export default defineConfig({
     plugins: [
         laravel({
             input: [
-                'resources/sass/app.scss', 
-                'resources/js/app.js'
-              ],
+                'resources/css/app.css',
+                'resources/js/app.js',
+            ],
             refresh: true,
         }),
     ],
